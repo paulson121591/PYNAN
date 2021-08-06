4b993512632a481730a4998006e151dc79fa235f
paulson121591 <46290543+paulson121591@users.noreply.github.com>
2021-08-06 19:03:36 -0400

    testing push

--- ToNotion.py
+++ ToNotion.py
@@ -24,7 +24,6 @@ def readDatabase(databaseId, headers):
 
     res = requests.request("POST", readUrl, headers=headers)
     data = res.json()
-    print(res.status_code)
 
     with open('./db.json', 'w', encoding='utf8') as f:
         json.dump(data, f, ensure_ascii=False)
